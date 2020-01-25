7ff8504be230162576d097a35586dc50a0c38093
moonjason <jasonmoonjm@gmail.com>
2020-01-24 23:50:15 -0800

    adjust navbar text

--- client/src/components/PageNav/PageNav.js
+++ client/src/components/PageNav/PageNav.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const PageNav = () => {
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar bg="light" expand="lg">
             <Navbar.Brand>Fino's</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center mr-auto"> 
+                    <Navbar.Text>Home</Navbar.Text>
+                    <Navbar.Text>Gallery</Navbar.Text>
+                    <Navbar.Text>About</Navbar.Text>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
