8e05827797892436ef656a2155557e9c1e5ab959
Jason Moon <jasonmoonjm@gmail.com>
2020-01-25 02:21:12 -0800

    adjust brand class padding

--- client/src/components/PageNav/PageNav.js
+++ client/src/components/PageNav/PageNav.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+
 const PageNav = () => {
     return (
             <Navbar bg="light" expand="md">
@@ -9,13 +10,13 @@ const PageNav = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center"> 
                         <Nav>
-                            <Nav.Item>
+                            <Nav.Item className="nav__item">
                                 <Nav.Link>Home</Nav.Link>
                             </Nav.Item>
-                            <Nav.Item>
+                            <Nav.Item className="nav__item">
                                 <Nav.Link>Gallery</Nav.Link>
                             </Nav.Item>
-                            <Nav.Item>
+                            <Nav.Item className="nav__item">    
                                 <Nav.Link>About</Nav.Link>
                             </Nav.Item>
                         </Nav>
