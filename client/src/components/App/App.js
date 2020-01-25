6c51804069d50a0f65b3d736e518af5b5fca26f1
moonjason <jasonmoonjm@gmail.com>
2020-01-24 22:10:48 -0800

    add bootstrap nav

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Header from '../Header/Header';
+import PageNav from '../PageNav/PageNav';
 import Home from '../Home/Home';
 import Gallery from '../Gallery/Gallery';
 import About from '../About/About';
@@ -9,7 +9,7 @@ import About from '../About/About';
 const App = () => {
   return (
     <div className="App">
-      <Header/>
+      <PageNav/>
       <Switch>
         <Route exact path={'/'} render={() => <Home/>}/>
         <Route exact path={'/gallery'} render={() => <Gallery/>}/>
---
+++ client/src/components/PageNav/PageNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+const PageNav = () => {
+    return (
+        <Navbar bg="light" variant="light">
+            <Navbar.Brand>Fino's</Navbar.Brand>
+        </Navbar>
+    )
+}
+
+export default PageNav;
