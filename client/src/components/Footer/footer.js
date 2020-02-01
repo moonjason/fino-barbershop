d6293caeab2c6db1f817eeced8d3159f434dbced
Jason Moon <jasonmoonjm@gmail.com>
2020-01-31 17:02:22 -0800

    add icons to footer

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -7,7 +7,11 @@ const Footer = () => {
         <div className="footer-container">
             <div className="footer__socials">
                 <h3 className="footer--connect"><em>Connect with Fino</em></h3>
-
+                <div className="footer__icons__container">
+                    <FontAwesomeIcon className="footer--icon" size="2x" icon={['fab', 'facebook']}/>
+                    <FontAwesomeIcon className="footer--icon" size="2x" icon={['fab', 'instagram']}/>
+                    <FontAwesomeIcon className="footer--icon" size="2x" icon={['fab', 'yelp']}/>
+                </div>
             </div>
         </div>
     )
