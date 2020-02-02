6b80e9ac237ee3d25fa449481cc8c0f71dbb3597
moonjason <jasonmoonjm@gmail.com>
2020-02-01 18:46:28 -0800

    adjust business card margin

--- client/src/components/Pricing/Pricing.js
+++ client/src/components/Pricing/Pricing.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Pricing = () => {
     return (
         <div id="pricing-container">
-            
+            <div id="pricing__carousel">
+                hi
+            </div>
+            <div id="pricing__text">
+                hi
+            </div>
         </div>
     )
 }
