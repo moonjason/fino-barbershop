45806801087e683f790663e4a839f0cf816425ec
moonjason <jasonmoonjm@gmail.com>
2020-02-02 18:22:59 -0800

    pushing update

--- client/src/components/Pricing/Pricing.js
+++ client/src/components/Pricing/Pricing.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Pricing = () => {
     return (
         <div id="pricing-container">
-            <div id="pricing__text">
+            <div id="pricing__texttwo">
                 <h1 className="pricing--title">Booking and Pricing</h1>
                 <h5>Text Fino at (213) 858-2325 for available times</h5>
                 <h5>Men's cuts are $25</h5>
             </div>
 
-            <div id="pricing__carousel">
+            <div id="pricing__carouseltwo">
                 <img className="photo" src="img/fino-shop.jpeg" alt="fino 1"/>
                 <img className="photo" src="img/fino-cut3.jpeg" alt="fino 2"/>
             </div>
