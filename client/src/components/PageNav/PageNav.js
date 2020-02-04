5f01b9f3c98dd80035849eb8738e7bbe911edd7a
Jason Moon <jasonmoonjm@gmail.com>
2020-02-03 19:04:29 -0800

    adjust camelcasing on css

--- client/src/components/PageNav/PageNav.js
+++ client/src/components/PageNav/PageNav.js
@@ -6,14 +6,14 @@ import { NavLink, Link } from 'react-router-dom';
 
 const navLinkActive = {
     "color": "#e50914",
-    "text-decoration": "none",
-    "border-bottom": "1px solid #e50914",
+    "textDecoration": "none",
+    "borderBottom": "1px solid #e50914",
 }
 
 const PageNav = () => {
     return (
-            <Navbar style={{"background-color": "#f5f5f1"}} expand="md">
-                    <Navbar.Brand className="brand"><Link className="link" exact to="/">Fino's</Link></Navbar.Brand>                        
+            <Navbar style={{"backgroundColor": "#f5f5f1"}} expand="md">
+                    <Navbar.Brand className="brand"><Link className="link" to="/">Fino's</Link></Navbar.Brand>                        
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center"> 
                         <Nav>
@@ -24,12 +24,12 @@ const PageNav = () => {
                             </Nav.Item>
                             <Nav.Item className="nav__item">
                                 <Nav.Link>
-                                    <NavLink className="nav--link" activeStyle={navLinkActive} exact to="/gallery">Gallery</NavLink>
+                                    <NavLink className="nav--link" activeStyle={navLinkActive} to="/gallery">Gallery</NavLink>
                                 </Nav.Link>
                             </Nav.Item>
                             <Nav.Item className="nav__item">    
                                 <Nav.Link>
-                                    <NavLink className="nav--link" activeStyle={navLinkActive} exact to="/about">About</NavLink>
+                                    <NavLink className="nav--link" activeStyle={navLinkActive} to="/about">About</NavLink>
                                 </Nav.Link>
                             </Nav.Item>
                         </Nav>
