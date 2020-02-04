738d3da0d4b651f882f1585d5c8fc8ad0d22fb10
Jason Moon <jasonmoonjm@gmail.com>
2020-02-03 20:30:48 -0800

    set up about me page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const About = () => {
     return(
-        <div>About</div>
+        <div id="about">
+            <h1 id="about__title">About Me</h1>
+            <img className="abt-image" src="img/fino-bmw1.jpeg" alt="fino bmw"/>
+        </div>
     )
 }
 
