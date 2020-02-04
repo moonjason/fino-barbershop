7957569e7562b687524e0d5cf561f7cf149c0fcc
Jason Moon <jasonmoonjm@gmail.com>
2020-02-03 19:30:39 -0800

    set up pricing flexboxes

--- client/src/components/Pricing/Pricing.js
+++ client/src/components/Pricing/Pricing.js
@@ -5,11 +5,24 @@ const Pricing = () => {
         <div id="pricing-containertwo">
             <div id="pricing__texttwo">
                 <h1 className="pricing--title">Booking and Pricing</h1>
-                <h5 className="pricing--desc">Haircut - $30</h5>
-                <h5 className="pricing--desc">Beard Trim - $20</h5>
-                <h5 className="pricing--desc">Haircut & Trim - $40</h5>
-                <h5 className="pricing--desc">Eyebrows - $15</h5>
-                <h5 className="pricing--desc" style={{'paddingTop': '25px'}}><i>Text Fino at (213) 858-2325 for availability</i></h5>
+
+                <div id="menu">
+                    <div id="pricing-desc-container">
+                        <h5 className="pricing--desc">Haircut</h5>
+                        <h5 className="pricing--desc">Beard Trim</h5>
+                        <h5 className="pricing--desc">Haircut & Beard Trim</h5>
+                        <h5 className="pricing--desc">Eyebrows</h5>
+                    </div>
+                    <div id="pricing-num-container">
+                        <h5 className="pricing--num">$30</h5>
+                        <h5 className="pricing--num">$20</h5>
+                        <h5 className="pricing--num">$40</h5>
+                        <h5 className="pricing--num">$15</h5>
+                    </div>
+                </div>
+
+
+                <h5 style={{'paddingTop': '25px', 'width': '20rem'}}><i>Text Fino at (213) 858-2325 for availability</i></h5>
             </div>
 
             <div id="pricing__carouseltwo">
