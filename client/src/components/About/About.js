ffdc7360ebf9c076589cbb2a44b9a6d9a4987fb0
Jason Moon <jasonmoonjm@gmail.com>
2020-02-09 16:00:56 -0800

    add about stuff

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -5,8 +5,15 @@ const About = () => {
         <div id="about">
             <h1 id="about__title">About Me</h1>
             <div id="msg-container">
-                <img className="abt-image" src="img/fino-bmw1.jpeg" alt="fino bmw"/>
-                
+                <div id="msg-container__1">
+                    <img className="abt-image" src="img/fino-bmw1.jpeg" alt="fino bmw"/>
+                </div>
+                <div id="msg-container__2">
+                    <div>In 2017, I discovered my passion for cutting hair and decided to provide my services to the public. It is extremely rewarding for me to send off a customer happy with their new fresh style as it motivates me to keep improving. 
+                    <br/>
+                    <br/>
+                    Being a barber allows me to channel one of my artistic ambitions as I am also very involved in music. As a side hustle, I'm on the grind to make a name in the music industry.</div>
+                </div>
             </div>
         </div>
     )
