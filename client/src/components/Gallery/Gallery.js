571b7365ca682c10bdd0acda4f623ab59cd39683
Jason Moon <jasonmoonjm@gmail.com>
2020-02-12 17:06:10 -0800

    add photos to gallery

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -6,20 +6,36 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
 const img = {
-    'max-width': '45vw'
+    'max-width': '35vw'
 }
 
 const Gallery = () => {
     return (
         <div id="gallery-container"> 
+            <h1 id="about__title">Gallery</h1>
             <Container>
                 <Row>
                     <Col>
-                        <Image style={img} src="img/fino-bmw2.jpeg" thumbnail/>
+                        <Image style={img} src="img/fino-bmw2.jpeg" fluid thumbnail/>
+                    </Col>
+                    <Col>
+                        <Image style={img} src="img/app-img.jpg" fluid thumbnail/>
                     </Col>
                 </Row>
-                <Row>
+                <Row style={{'paddingTop': '1rem'}}>
                     <Col>
+                        <Image style={img} src="img/fino-bmw1.jpeg" fluid thumbnail/>
+                    </Col>
+                    <Col>
+                        <Image style={img} src="img/fino-cut1.jpeg" fluid thumbnail/>
+                    </Col>
+                </Row>
+                <Row style={{'paddingTop': '1rem'}}>
+                    <Col>
+                        <Image style={img} src="img/fino-cut2.jpeg" fluid thumbnail/>
+                    </Col>
+                    <Col>
+                        <Image style={img} src="img/fino-cut3.jpeg" fluid thumbnail/>
                     </Col>
                 </Row>
             </Container>
